e69a70c06b7dbeed1543d6b5789885913c1e6b88
Jason <tea.jason@gmail.com>
2016-01-31 10:26:27 -0800

    updated flightsQuery to return dictionary

--- testBed/questTest.py
+++ testBed/questTest.py
@@ -1,8 +1,11 @@
 import json
 import requests
 
-
+depCode = "SEA"
 maxDays = 10
+all_destinations = ["SFO", "LAX", "JFK"]
+
+
 url = 'http://terminal2.expedia.com:80/x/flights/overview/get'
 data = {
 "MessageHeader" : {
@@ -27,13 +30,13 @@ data = {
 "ManagementUnitCode" : "1010"
 },
 "OriginAirportCodeList" : {
-"AirportCode" : [ "SEA" ]
+"AirportCode" : [ depCode ]
 },
 "DestinationAirportCodeList" : {
-"AirportCode" : [ "SFO" ]
+"AirportCode" : [ des ]
 },
 "FlightListings" : {
-"MaxCount" : 10
+"MaxCount" : 20
 },
 "FareCalendar": {
 "StartDate": "2016-02-05T19:33:39.363-08:00",
@@ -45,16 +48,25 @@ data_json = json.dumps(data)
 headers = {'accept': 'application/json', 'Authorization': 'expedia-apikey key=BQBh6sGziLeQsNQxVjHPlaO08ATfLKn7'}
 response = requests.post(url, data=data_json, headers=headers)
 
-fakeReturn = response.json()
-totalItems = len(fakeReturn["FareCalendar"]["AirOfferSummary"])
-print("totalItems is: " + str(totalItems))
+jsonReturn = response.json()
+totalItems = len(jsonReturn["FareCalendar"]["AirOfferSummary"])
 allFlights = []
+allFlightsDepDates = []
+allFlightsRetDates = []
 for i in range(0,totalItems):
-    allFlights.append(fakeReturn["FareCalendar"]["AirOfferSummary"][i]["FlightPriceSummary"]["TotalPrice"])
-
-
-with open('results.txt','w',) as outfile:
-    json.dump(response.json(),outfile)
+    allFlights.append(jsonReturn["FareCalendar"]["AirOfferSummary"][i]["FlightPriceSummary"]["TotalPrice"])
+    allFlightsDepDates.append(jsonReturn["FareCalendar"]["AirOfferSummary"][i]["FlightItinerarySummary"]["OutboundDepartureTime"])
+    allFlightsRetDates.append(jsonReturn["FareCalendar"]["AirOfferSummary"][i]["FlightItinerarySummary"]["InboundDepartureTime"])
 
 resultMin = min(allFlights)
-print(resultMin)
+indexOfMin = allFlights.index(price)
+
+flightsInfo = {}
+flightsInfo["depDate"] = allFlightsDepDates[index]
+flightsInfo["retDate"] = allFlightsRetDates[index]
+flightsInfo["price"] = allFlights[index]
+
+flightsDict = {}
+for des in all_destinations:
+    flightsDict[des] = flightsQuery(depCode, des, maxDays)
+
